3560a800b4461c01c370078357407e242074b903
Alan Hardman <alanaktion@gmail.com>
2015-01-11 00:08:22 -0700

    Adding empty views for later use

--- base.php
+++ base.php
@@ -16,11 +16,12 @@ class Base extends \Plugin {
 		$f3 = \Base::instance();
 
 		// Add menu item
-		$this->_addNav("wiki", $f3->get("dict.wiki.wiki"), "/\\/wiki/i");
+		$this->_addNav("wiki", $f3->get("dict.wiki.wiki"), '/$\\/wiki/i');
 
 		// Add routes
 		$f3->route("GET /wiki", "Plugin\Wiki\Controller->index");
 		$f3->route("GET /wiki/@page", "Plugin\Wiki\Controller->single");
+		$f3->route("GET|POST /wiki/edit/@page", "Plugin\Wiki\Controller->edit");
 	}
 
 	/**
@@ -42,6 +43,26 @@ class Base extends \Plugin {
 		$db = $f3->get("db.instance");
 		$q = $db->exec("SHOW TABLES LIKE 'wiki_page'");
 		return !!$db->count();
+	}
+
+	/**
+	 * Generate page for admin panel
+	 */
+	public function _admin() {
+		$f3 = \Base::instance();
+		$db = $f3->get("db.instance");
+
+		// Gather some stats
+		$result = $db->exec("SELECT COUNT(id) AS `count` FROM wiki_page WHERE deleted_date IS NULL");
+		$f3->set("count_page", $result[0]["count"]);
+		$result = $db->exec("SELECT COUNT(DISTINCT user_id) AS `count` FROM wiki_page_update");
+		$f3->set("count_page_update_user", $result[0]["count"]);
+		$result = $db->exec("SELECT COUNT(id) AS `count` FROM wiki_page_update");
+		$f3->set("count_page_update", $result[0]["count"]);
+
+		// Render view
+		$f3->set("UI", $f3->get("UI") . ";./app/plugin/wiki/view/");
+		echo \Helper\View::instance()->render("admin/plugin-wiki.html");
 	}
 
 }
--- controller.php
+++ controller.php
@@ -10,16 +10,37 @@ namespace Plugin\Wiki;
 class Controller extends \Controller {
 
 	public function index($f3) {
-		$this->single($f3, array("page" => "index"));
+		$f3->set("PARAMS", array("page" => "index"));
+		$this->single($f3, $f3->get("PARAMS"));
 	}
 
 	public function single($f3, $params) {
 		$page = new Model\Page;
 		$page->load(array("slug = ?", $params["page"]));
+
+		$f3->set("UI", $f3->get("UI") . ";./app/plugin/wiki/view/");
+
 		if(!$page->id) {
-			$f3->error(404);
+			$f3->status(404);
+			$this->_render("wiki/404.html");
 			return;
 		}
+
+		$f3->set("page", $page);
+		$this->_render("wiki/single.html");
+	}
+
+	public function edit($f3, $params) {
+		if($f3->get("POST")) {
+			// TODO: Save posted edit
+		}
+
+		$page = new Model\Page;
+		$page->load(array("slug = ?", $params["page"]));
+
+		$f3->set("page", $page);
+		$f3->set("UI", $f3->get("UI") . ";./app/plugin/wiki/view/");
+		$this->_render("wiki/edit.html");
 	}
 
 }
